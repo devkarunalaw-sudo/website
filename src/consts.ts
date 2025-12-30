80f68cb07247184c59e239564b249d82bbf9982a
thupten tsundue <thups1951@gmail.com>
2025-12-30 00:27:26 -0500

    path variable fix

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+export const SITE_NAME = 'Karuna Law';
+export const SITE_URL = 'https://devkarunalaw-sudo.github.io/website';
+
+// Automatically resolves to '/' locally and '/website/' on GitHub Pages
+export const BASE_URL = import.meta.env.BASE_URL;

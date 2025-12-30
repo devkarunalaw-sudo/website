c157009c1b94c57106dbcbe4d32a33f082363f80
thupten tsundue <thups1951@gmail.com>
2025-12-29 21:22:19 -0500

    dev ops

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
-
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://karuna-law.github.io',
   vite: {
     plugins: [tailwindcss()]
   },

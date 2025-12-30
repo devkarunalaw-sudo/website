12df159d1f257c7d9e444d90252897d6f72b8b68
thupten tsundue <thups1951@gmail.com>
2025-12-30 06:58:13 -0500

    removed site and base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,6 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://devkarunalaw-sudo.github.io",
-  base: "/website",
   vite: {
     plugins: [tailwindcss()],
   },

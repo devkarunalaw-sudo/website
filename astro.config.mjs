b850e01048d8bff3bb543d32c58bb9a24894f1d4
thupten tsundue <thups1951@gmail.com>
2025-12-29 23:53:32 -0500

    Fix base path for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
-import mdx from '@astrojs/mdx';
+import tailwindcss from "@tailwindcss/vite";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://karuna-law.github.io',
+  site: "https://devkarunalaw-sudo.github.io/website",
+  base: "/website/",
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
-
-  integrations: [mdx()]
+  integrations: [mdx()],
 });

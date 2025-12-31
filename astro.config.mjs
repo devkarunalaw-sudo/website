ea6c3a16fbb440e9d7d75462140139f4c501a325
thupten tsundue <thups1951@gmail.com>
2025-12-31 11:50:06 -0500

    added base

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-// astro.config.mjs
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 
 export default defineConfig({
   site: "https://www.karunalaw.ca",
+  base: "/", // Add this line for custom domain
   vite: {
     plugins: [tailwindcss()],
   },
